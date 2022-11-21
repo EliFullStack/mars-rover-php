775aa8ac005efd0e54f4867dfecceb0825336659
unknown <elisabet.v.alvarez@gmail.com>
2022-11-21 15:39:14 +0100

    add tests and changes in composer.json

---
+++ tests/PlanetTest.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class PlanetTest extends TestCase {
+
+    public $planet;
+
+    public function setUp(): void {
+        $this->planet = new Planet();
+    }
+
+}
+
+
+
+?>
---
+++ tests/RoverTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class RoverTest extends TestCase {
+    
+    public $rover;
+
+    public function setUp(): void {
+        $this->rover = new Rover();
+    }
+    
+}
+
+?>
