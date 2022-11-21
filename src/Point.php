f59e9200014e1296d0f89f4ffb20fb7d1e75cdaa
unknown <elisabet.v.alvarez@gmail.com>
2022-11-21 15:03:25 +0100

    create Point class

---
+++ src/Point.php
@@ -0,0 +1,21 @@
+<?php
+
+class Point {
+
+    public static $x;
+
+    public static $y;
+
+    function getX()
+    {
+        return self::$x;
+    } 
+
+    function getY()
+    {
+        return self::$y;
+    } 
+
+}
+
+?>
